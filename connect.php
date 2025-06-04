d58103c205d61887209bff4d1d3e87fa5dfa639d
gitaelizza <gitaelizza@gmail.com>
2025-06-04 21:29:42 +0700

    fix CD

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
 // Database connection
-DEFINE ('DB_USER', 'recipeadmin');
-DEFINE ('DB_PASSWORD', 'kod12345');
-DEFINE ('DB_HOST', 'host.docker.internal');
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '@<0zAacK_Guyj7{6');
+DEFINE ('DB_HOST', '34.50.70.228');
 DEFINE ('DB_NAME', 'recipedb');
 
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, 3306) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
